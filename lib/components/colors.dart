1b626cc25cdf7df0c83f6340943e427968e7b32a
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-11 19:18:45 +0300

    The folder for app components

---
+++ lib/components/colors.dart
@@ -0,0 +1,7 @@
+import 'package:hexcolor/hexcolor.dart';
+
+class GlobalColors {
+  static HexColor mainColor = HexColor('#1a9283');
+  static HexColor whiteColor = HexColor('#ffffff');
+  static HexColor blackColor = HexColor('#020e0d');
+}
