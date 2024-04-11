606210a3bfadbb8ec15c2f10e3ddac6590e946ab
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-11 20:32:46 +0300

    Starting the bottom app bar using awesome bottom bar package

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/components/colors.dart';
+import 'package:whatsapp_clone/widgets/app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,18 +9,49 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("WhatsApp"),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.camera_alt_outlined),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: myAppBar(),
+      ),
+      bottomNavigationBar: BottomBarFloating(
+        indexSelected: 0,
+        animated: true,
+        items: [
+          TabItem(
+            icon: Icons.chat,
+            title: "Chats",
+            count: Badge(
+              label: Text("20"),
+              backgroundColor: GlobalColors.greenColor,
+              textColor: GlobalColors.whiteColor,
+            ),
           ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search_outlined),
+          TabItem(
+            icon: Icons.update,
+            title: "Updates",
+            count: Badge(
+              label: Text("31"),
+              backgroundColor: GlobalColors.greenColor,
+              textColor: GlobalColors.whiteColor,
+            ),
+          ),
+          const TabItem(
+            icon: Icons.people_outline,
+            title: "Communities",
+          ),
+          TabItem(
+            icon: Icons.call_outlined,
+            title: "Calls",
+            count: Badge(
+              label: Text("4"),
+              backgroundColor: GlobalColors.greenColor,
+              textColor: GlobalColors.whiteColor,
+            ),
           ),
         ],
+        backgroundColor: GlobalColors.whiteColor,
+        color: GlobalColors.blackColor,
+        colorSelected: GlobalColors.blackColor,
       ),
     );
   }
