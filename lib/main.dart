2a97e5fe9a4b9adb07742cf1e96e84c64d78713e
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-11 23:45:06 +0300

    Correcting the main color for the app

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WhatsApp Clone',
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: GlobalColors.mainColor),
           useMaterial3: true,
           appBarTheme: AppBarTheme(
             backgroundColor: GlobalColors.mainColor,
