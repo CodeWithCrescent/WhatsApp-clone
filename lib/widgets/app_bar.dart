e6e38dbe3816d16f2a99be0a362df998604774b6
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-11 23:44:05 +0300

    Making the appBar reusable

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,40 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_clone/components/colors.dart';
 
-Widget buildAppBar() {
+Widget buildAppBar(String title, List<Widget> actions) {
   return AppBar(
-    title: const Text("WhatsApp"),
-    actions: [
-      IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.camera_alt_outlined),
-      ),
-      IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.search_outlined),
-      ),
-      PopupMenuButton(
-        position: PopupMenuPosition.under,
-        offset: const Offset(0, 5),
-        surfaceTintColor: GlobalColors.bgColor,
-        itemBuilder: (context) => [
-          const PopupMenuItem(
-            child: Text('New group'),
-          ),
-          const PopupMenuItem(
-            child: Text('New broadcast'),
-          ),
-          const PopupMenuItem(
-            child: Text('Linked devices'),
-          ),
-          const PopupMenuItem(
-            child: Text('Starred messages'),
-          ),
-          const PopupMenuItem(
-            child: Text('Settings'),
-          ),
-        ],
-      ),
-    ],
+    title: Text(title),
+    actions: actions,
   );
 }
