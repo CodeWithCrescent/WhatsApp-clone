14ee0b00065e762c94fb6bd9bcdf177b510388ac
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-11 19:19:31 +0300

    The folder for all the user screen views

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/components/colors.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("WhatsApp"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.camera_alt_outlined),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+}
