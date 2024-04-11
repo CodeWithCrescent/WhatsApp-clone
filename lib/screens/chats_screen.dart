0c791f71a195554424f273d307f8c30bfcc045d1
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-11 23:43:36 +0300

    Using the common appBar from created AppBar widget

--- lib/screens/chats_screen.dart
+++ lib/screens/chats_screen.dart
@@ -5,8 +5,39 @@ class Chats extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Chats are to appear here."),
+    return Column(
+      children: [
+        Expanded(
+          // Wrap the ListView in Expanded for scrolling
+          child: ListView.builder(
+            itemCount: 30, // Set item count to 30
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: Row(
+                children: [
+                  Container(
+                    color: Colors.red,
+                    width: MediaQuery.of(context).size.width *
+                        1 /
+                        3, // Set width to 1/3 screen size
+                    height: 50, // Set row height (optional)
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    width: MediaQuery.of(context).size.width * 1 / 3,
+                    height: 50, // Set row height (optional)
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: MediaQuery.of(context).size.width * 1 / 3,
+                    height: 50, // Set row height (optional)
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
