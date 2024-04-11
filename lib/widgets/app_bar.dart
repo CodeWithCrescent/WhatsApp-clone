5a96a916b09b4e5a8317ce53936e06366e7721f3
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-11 20:30:37 +0300

    Adding the appbar as widget

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/components/colors.dart';
+
+Widget myAppBar() {
+  return AppBar(
+    title: const Text("WhatsApp"),
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.camera_alt_outlined),
+      ),
+      IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.search_outlined),
+      ),
+      PopupMenuButton(
+        position: PopupMenuPosition.under,
+        offset: const Offset(0, 5),
+        surfaceTintColor: GlobalColors.bgColor,
+        itemBuilder: (context) => [
+          const PopupMenuItem(
+            child: Text('New group'),
+          ),
+          const PopupMenuItem(
+            child: Text('New broadcast'),
+          ),
+          const PopupMenuItem(
+            child: Text('Linked devices'),
+          ),
+          const PopupMenuItem(
+            child: Text('Starred messages'),
+          ),
+          const PopupMenuItem(
+            child: Text('Settings'),
+          ),
+        ],
+      ),
+    ],
+  );
+}
