178f95aad370151840f82197325a98de65a31664
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-11 20:31:26 +0300

    Creating the colors component file

--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -2,6 +2,8 @@ import 'package:hexcolor/hexcolor.dart';
 
 class GlobalColors {
   static HexColor mainColor = HexColor('#1a9283');
+  static HexColor bgColor = HexColor('#ffffff');
   static HexColor whiteColor = HexColor('#ffffff');
   static HexColor blackColor = HexColor('#020e0d');
+  static HexColor greenColor = HexColor('#25D366');
 }
